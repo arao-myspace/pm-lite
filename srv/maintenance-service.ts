25dd07487d11effd622bce4492cf599a5567e064
Adrian R <adrian.rybka1989@gmail.com>
2026-09-25 21:25:05 +0200

    New validation added to check if the equipment exists in the system before creating a notification. If the equipment does not exist, a 400 error is returned with a message indicating that the specified machine does not exist in the system.

--- srv/maintenance-service.ts
+++ srv/maintenance-service.ts
@@ -4,10 +4,11 @@ export default class MaintenanceService extends cds.ApplicationService {
     async init() {
 
         // 1. Wyciągamy encję z definicji usługi w pliku .cds
-        const { NotificationsService } = this.entities;
+        const { NotificationsService, EquipmentService } = this.entities;
 
-        if (!NotificationsService) {
-            throw new Error('NotificationsService entity was not found');
+        // Sprawdzamy czy obie encje istnieją w modelu
+        if (!NotificationsService || !EquipmentService) {
+            throw new Error('Wymagane encje nie zostały znalezione w modelu CDS');
         }
         this.before('CREATE', NotificationsService, async (req: cds.Request) => {
 
@@ -18,6 +19,10 @@ export default class MaintenanceService extends cds.ApplicationService {
                 payload.faultDescription = '[PILNE]' + payload.faultDescription;
             }
 
+            const result = await SELECT.one.from(EquipmentService).where({ ID: payload.equipment_ID });
+            if (!result) {
+                req.error(400, 'Podana maszyna nie istnieje w systemie!', payload.equipment_ID);
+            }
         });
 
         return super.init(); // To zawsze musi być na końcu metody init!
